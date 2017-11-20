7d57c4c7f0691cedd800eefb160fd9bbf922af1d
同程网络-彭笳鑫48476 <pjx48476@ly.com>
2017-11-20 13:49:03 +0800

    0.16

--- script/model_predict.py
+++ script/model_predict.py
@@ -9,11 +9,13 @@ from sklearn.metrics import recall_score, precision_score
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
 
+e = 0.8
+
 def train_xgb(data, labels, test=None, test_label=None):
     param = {}
     param['objective'] = 'binary:logistic'
-    param['eta'] = 0.5
-    param['max_depth'] = 5
+    param['eta'] = 0.25
+    param['max_depth'] = 3
     param['eval_metric'] = "auc"
     param['min_child_weight'] = 3
     param['subsample'] = 0.8
@@ -35,7 +37,7 @@ def train_xgb(data, labels, test=None, test_label=None):
     xgtest = xgb.DMatrix(test_X, label=test_y)
 
     watchlist = [(xgtrain, 'train'), (xgtest, 'eval')]
-    bst = xgb.train(param, xgtrain, num_rounds, watchlist, maximize=True, feval=ff,  early_stopping_rounds=20)
+    bst = xgb.train(param, xgtrain, num_rounds, watchlist, maximize=True,  early_stopping_rounds=40)
     importance = bst.get_fscore()
     print(sorted(importance.items()))
 
@@ -43,8 +45,8 @@ def train_xgb(data, labels, test=None, test_label=None):
     preds = bst.predict(xgtrain)
     test['is_risk'] = pd.DataFrame(preds)
 
-    test.loc[test['is_risk'] > 0.6, 'is_risk'] = 1
-    test.loc[test['is_risk'] <= 0.6, 'is_risk'] = 0
+    test.loc[test['is_risk'] > e, 'is_risk'] = 1
+    test.loc[test['is_risk'] <= e, 'is_risk'] = 0
     test = pd.concat([test, member],axis=1)
     test = test_label.merge(test[test['is_risk'] > 0][['rowkey', 'is_risk']].drop_duplicates(), how='left', on='rowkey')
     test['is_risk'] = test.is_risk.fillna(0)
@@ -54,7 +56,7 @@ def train_xgb(data, labels, test=None, test_label=None):
 
 def ff(preds, dtrain):
     label = dtrain.get_label()
-    preds = map(lambda x: 1 if x > 0.8 else 0, preds)
+    preds = map(lambda x: 1 if x > e else 0, preds)
     return 'fbeta_score', fbeta_score(label, preds, beta=0.1)
 
 train=pd.read_csv('../input/t_login.csv')
@@ -63,17 +65,33 @@ t_test = pd.read_csv('../input/t_trade_test.csv')
 t_train = pd.read_csv('../input/t_login_test.csv')
 
 train_test = train[(train.time < '2015-06-01')]
-test_test = test[(test.time > '2015-05-01') & (test.time < '2015-06-01')]
+test_test = test[(test.time >= '2015-05-01') & (test.time < '2015-06-01')]
 
 print test_test.is_risk.value_counts()
 
-feature_to_use = ['rowkey', 'result','timelong','device','log_from','ip','city','id', 'is_scan', 'type']
+all_train = pd.concat([train, t_train])
+
+devices= train_test[['id','device']].drop_duplicates().groupby(['id'])['device'].count().reset_index()
+all_devices = all_train[['id','device']].drop_duplicates().groupby(['id'])['device'].count().reset_index()
+devices.columns=['id', 'divs']
+all_devices.columns=['id', 'divs']
+train_test = train_test.merge(devices, on='id')
+all_train = all_train.merge(all_devices, on='id')
+
+citys= train_test[['id','city']].drop_duplicates().groupby(['id'])['city'].count().reset_index()
+all_citys = all_train[['id','city']].drop_duplicates().groupby(['id'])['city'].count().reset_index()
+citys.columns=['id', 'citys']
+all_citys.columns=['id', 'citys']
+train_test = train_test.merge(citys, on='id')
+all_train = all_train.merge(all_citys, on='id')
+
+feature_to_use = ['rowkey', 'divs', 'citys', 'id']
+# feature_to_use = ['rowkey', 'result','timelong','device','log_from','ip','city','id', 'is_scan', 'type']
 # train = pd.concat([train, pd.get_dummies(train['is_scan'], prefix='scan'), pd.get_dummies(train['type'], prefix='type'), pd.get_dummies(train['result'], prefix='result')], axis=1)
 # _train = pd.concat([t_train, pd.get_dummies(t_train['is_scan'], prefix='scan'), pd.get_dummies(t_train['type'], prefix='type'), pd.get_dummies(t_train['result'], prefix='result')], axis=1)
 
-all = train.merge(test, on='id')
-_all = t_train.merge(t_test, on='id')
+all = train_test.merge(test_test, on='id')
+_all = all_train.merge(t_test, on='id')
 
-t_all= pd.concat([all, _all])
 all['is_risk'] = all.is_risk.astype(int)
-train_xgb(all[feature_to_use], all['is_risk'], t_all[feature_to_use], test_label=t_test)
+train_xgb(all[feature_to_use], all['is_risk'], _all[feature_to_use], test_label=t_test)
