a82774489daa6aa5ceca6e23cf0780bdf3d64c0e
同程网络-彭笳鑫48476 <pjx48476@ly.com>
2017-11-21 18:40:27 +0800

    0.25

--- script/model_predict.py
+++ script/model_predict.py
@@ -9,13 +9,13 @@ from sklearn.metrics import recall_score, precision_score
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
 
-e = 0.8
+e = 0.5
 
 def train_xgb(data, labels, test=None, test_label=None):
     param = {}
     param['objective'] = 'binary:logistic'
-    param['eta'] = 0.25
-    param['max_depth'] = 3
+    param['eta'] = 0.3
+    param['max_depth'] = 5
     param['eval_metric'] = "auc"
     param['min_child_weight'] = 3
     param['subsample'] = 0.8
@@ -40,9 +40,10 @@ def train_xgb(data, labels, test=None, test_label=None):
     bst = xgb.train(param, xgtrain, num_rounds, watchlist, maximize=True,  early_stopping_rounds=40)
     importance = bst.get_fscore()
     print(sorted(importance.items()))
+    bst.dump_model("dump.raw.txt")
 
     xgtrain = xgb.DMatrix(test)
-    preds = bst.predict(xgtrain)
+    preds = bst.predict(xgtrain, ntree_limit=bst.best_ntree_limit)
     test['is_risk'] = pd.DataFrame(preds)
 
     test.loc[test['is_risk'] > e, 'is_risk'] = 1
@@ -52,7 +53,7 @@ def train_xgb(data, labels, test=None, test_label=None):
     test['is_risk'] = test.is_risk.fillna(0)
     test['is_risk'] = test.is_risk.astype(int)
     print test[['rowkey', 'is_risk']].drop_duplicates().is_risk.value_counts()
-    test[['rowkey', 'is_risk']].to_csv("preds.csv", index=False, header=False)
+    test[['rowkey', 'is_risk']].to_csv("preds2.csv", index=False, header=False)
 
 def ff(preds, dtrain):
     label = dtrain.get_label()
@@ -64,34 +65,57 @@ test=pd.read_csv('../input/t_trade.csv')
 t_test = pd.read_csv('../input/t_trade_test.csv')
 t_train = pd.read_csv('../input/t_login_test.csv')
 
-train_test = train[(train.time < '2015-06-01')]
+train_test = train[(train.time < '2015-05-01') & (train.time >= '2015-02-01')]
 test_test = test[(test.time >= '2015-05-01') & (test.time < '2015-06-01')]
 
-print test_test.is_risk.value_counts()
 
-all_train = pd.concat([train, t_train])
+all_train = pd.concat([train_test, t_train])
 
 devices= train_test[['id','device']].drop_duplicates().groupby(['id'])['device'].count().reset_index()
 all_devices = all_train[['id','device']].drop_duplicates().groupby(['id'])['device'].count().reset_index()
 devices.columns=['id', 'divs']
 all_devices.columns=['id', 'divs']
-train_test = train_test.merge(devices, on='id')
-all_train = all_train.merge(all_devices, on='id')
+test_test = test_test.merge(devices, on='id')
+all_t_test = t_test.merge(all_devices, on='id')
 
 citys= train_test[['id','city']].drop_duplicates().groupby(['id'])['city'].count().reset_index()
 all_citys = all_train[['id','city']].drop_duplicates().groupby(['id'])['city'].count().reset_index()
 citys.columns=['id', 'citys']
 all_citys.columns=['id', 'citys']
-train_test = train_test.merge(citys, on='id')
-all_train = all_train.merge(all_citys, on='id')
+test_test = test_test.merge(citys, on='id')
+all_t_test = all_t_test.merge(all_citys, on='id')
 
-feature_to_use = ['rowkey', 'divs', 'citys', 'id']
+ips = train_test[['id','ip']].drop_duplicates().groupby(['id'])['ip'].count().reset_index()
+all_ips = all_train[['id','ip']].drop_duplicates().groupby(['id'])['ip'].count().reset_index()
+ips.columns = ['id', 'ips']
+all_ips.columns = ['id', 'ips']
+test_test = test_test.merge(ips, on='id')
+all_t_test = all_t_test.merge(all_ips, on='id')
+
+log_ids = train_test[['id','log_id']].drop_duplicates().groupby(['id'])['log_id'].count().reset_index()
+all_log_ids = all_train[['id','log_id']].drop_duplicates().groupby(['id'])['log_id'].count().reset_index()
+log_ids.columns = ['id', 'log_ids']
+all_log_ids.columns = ['id', 'log_ids']
+test_test = test_test.merge(log_ids, on='id')
+all_t_test = all_t_test.merge(all_log_ids, on='id')
+
+log_froms = train_test[['id','log_from']].drop_duplicates().groupby(['id'])['log_from'].count().reset_index()
+all_log_froms = all_train[['id','log_from']].drop_duplicates().groupby(['id'])['log_from'].count().reset_index()
+log_froms.columns = ['id', 'log_froms']
+all_log_froms.columns = ['id', 'log_froms']
+test_test = test_test.merge(log_froms, on='id')
+all_t_test = all_t_test.merge(all_log_froms, on='id')
+
+feature_to_use = ['rowkey', 'divs', 'citys', 'ips', 'log_ids','id', 'log_froms']
 # feature_to_use = ['rowkey', 'result','timelong','device','log_from','ip','city','id', 'is_scan', 'type']
 # train = pd.concat([train, pd.get_dummies(train['is_scan'], prefix='scan'), pd.get_dummies(train['type'], prefix='type'), pd.get_dummies(train['result'], prefix='result')], axis=1)
 # _train = pd.concat([t_train, pd.get_dummies(t_train['is_scan'], prefix='scan'), pd.get_dummies(t_train['type'], prefix='type'), pd.get_dummies(t_train['result'], prefix='result')], axis=1)
 
-all = train_test.merge(test_test, on='id')
-_all = all_train.merge(t_test, on='id')
+# all = train_test.merge(test_test, on='id')
+# _all = all_train.merge(t_test, on='id')
+
+all = test_test
+_all = all_t_test
 
 all['is_risk'] = all.is_risk.astype(int)
 train_xgb(all[feature_to_use], all['is_risk'], _all[feature_to_use], test_label=t_test)
--- script/step3.py
+++ script/step3.py
@@ -8,7 +8,7 @@ t_train = pd.read_csv('../input/t_login_test.csv')
 t_test = pd.read_csv('../input/Risk_Detection_Upload_Sample.csv', header=-1)
 t_test.columns=['rowkey', 'is_risk']
 
-preds = pd.read_csv('preds.csv', header=-1)
+preds = pd.read_csv('preds2.csv', header=-1)
 preds.columns=['rowkey', 'is_risk']
 t_test = t_test[['rowkey']].merge(preds[preds['is_risk']>0], how='left', on='rowkey')
 t_test['is_risk'] = t_test.is_risk.fillna(0)
